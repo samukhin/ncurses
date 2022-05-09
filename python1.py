8d01465ec5be22921bf2976752e6644e5180b9ed
samukhin <47834890+samukhin@users.noreply.github.com>
2022-05-09 14:36:25 +0300

    Update python1.py

--- python1.py
+++ python1.py
@@ -12,16 +12,19 @@ curses.init_pair(1,0,0)
 curses.init_pair(2,7,0)
 screen.clear
 
-while 1: 
-    b = (b + 1)%2
-    screen.addstr(  (random.randint(0,height))%height,
-                    (random.randint(0,width))%width,
-                    '■',
-                    curses.color_pair(b))
-    
-    screen.timeout(10)
-    screen.refresh()
-    if (screen.getch()!=-1):
-        break
+while 1:
+    try:
+        b = (b + 1)%2
+        screen.addstr(  (random.randint(0,height))%height,
+                        (random.randint(0,width))%width,
+                        '■',
+                        curses.color_pair(b))
+
+        screen.timeout(10)
+        screen.refresh()
+        if (screen.getch()!=-1):
+            break
+    except:
+        pass
 
 curses.endwin()
