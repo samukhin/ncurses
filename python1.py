23dd771eaf810f3a0dcd28380ffc0779d582af68
samukhin <47834890+samukhin@users.noreply.github.com>
2022-05-09 14:34:49 +0300

    Create python1.py

---
+++ python1.py
@@ -0,0 +1,27 @@
+import curses, random
+
+screen  = curses.initscr()
+width   = screen.getmaxyx()[1]
+height  = screen.getmaxyx()[0]
+size    = width*height
+b       = 0
+
+curses.curs_set(0)
+curses.start_color()
+curses.init_pair(1,0,0)
+curses.init_pair(2,7,0)
+screen.clear
+
+while 1: 
+    b = (b + 1)%2
+    screen.addstr(  (random.randint(0,height))%height,
+                    (random.randint(0,width))%width,
+                    '■',
+                    curses.color_pair(b))
+    
+    screen.timeout(10)
+    screen.refresh()
+    if (screen.getch()!=-1):
+        break
+
+curses.endwin()
